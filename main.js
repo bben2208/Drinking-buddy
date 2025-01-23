44e71d7c7def61b771af2d11e6ad13171779446b
Benjamin Bognar <benjaminbognar@MacBook-Pro-2.lan>
2025-01-23 15:37:03 +0000

    update

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded');
 
     const drinksContainer = document.getElementById('drinksContainer');
+    const closeButton = document.getElementById('close-button')
 
     // Dropdown ingredient links
     const dropdownLinks = document.querySelectorAll('.dropdown-link');
     dropdownLinks.forEach(link => {
         link.addEventListener('click', async (event) => {
             const ingredient = event.target.dataset.ingredient;
+            closeButton.style.display = 'block';
             console.log(`Fetching drinks for ingredient: ${ingredient}`);
             if (!ingredient) return;
+            debugger
 
             try {
                 const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
                 const data = await response.json();
-
+console.log(data)
                 if (data.drinks) {
                     drinksContainer.innerHTML = '';
                     data.drinks.forEach(drink => {
@@ -34,6 +37,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 drinksContainer.innerHTML = `<p>Error loading drinks. Please try again later.</p>`;
             }
         });
+    });
+
+    closeButton.addEventListener('click', () =>{ 
+        drinksContainer.innerHTML = ''
+        closeButton.style.display = 'none';
     });
 
     // Navbar button event listeners
